602147a5b2aeb75b0770febeda85ecb7c063a96c
Ubuntu <ubuntu@ip-172-31-20-91.us-west-2.compute.internal>
2013-07-22 03:38:58 +0000

    index.html updated with bootstrap css for hw4 part1

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var fs = require('fs');
-
+var htmlfile = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', 
+  fs.readFile(htmlfile, 
 	function (err, data) {
   		if (err) throw err;
-  		response.send(data);
+  		//console.log("Serving Content from index.html:\n"+data);
+  		response.send(data.toString());
 	});
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
